c262fec55a4b2aa91bdff68aed3b5c78f3c23671
Helloguy777 <askhatbaev@outlook.com>
2025-05-06 17:16:42 -0400

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -4,16 +4,18 @@ module.exports = async (req, res) => {
   const url = req.query.url;
   if (!url) return res.status(400).send('Missing URL');
 
+  
+  const proxyUrl = `http://152.26.229.52:9443/${url}`;  
+
   try {
-    
-    const response = await fetch(url);
+    const response = await fetch(proxyUrl);
     const contentType = response.headers.get('content-type') || 'text/html';
 
     
     res.setHeader('Content-Type', contentType);
     const data = await response.text();
 
-   
+    
     res.send(data);
   } catch (err) {
     res.status(500).send('Proxy error');
