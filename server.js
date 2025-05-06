2f0e4a32b1793f893d54c3cf2e7aeb73408e4dfb
Helloguy777 <askhatbaev@outlook.com>
2025-05-06 18:00:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const proxy = require('./proxy'); 
+const proxy = require('./proxy');
 
-
-app.use(express.static('public'));
 app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
 app.use('/styles', express.static(path.join(__dirname, 'styles')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/icons', express.static(path.join(__dirname, 'icons')));
 
-
 app.get('/proxy', proxy);
-
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
